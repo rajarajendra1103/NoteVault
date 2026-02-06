df06a49c355d74af7097975e37fbfed5f75108bd
rajarajendra1103 <rajarajendraprasad123@gmail.com>
2026-02-06 21:20:24 +0530

    chore: update .gitignore to exclude .next, env files, and build artifacts

--- .next/dev/server/chunks/ssr/Downloads_notevault__next-internal_server_app_page_actions_a2033b5c.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Downloads/notevault/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=Downloads_notevault__next-internal_server_app_page_actions_a2033b5c.js.map
--- .next/dev/static/chunks/2ba1d_next_dist_client_components_builtin_global-error_aaf88cba.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/2ba1d_next_dist_ebf05f5a._.js"
-],
-    source: "dynamic"
-});
